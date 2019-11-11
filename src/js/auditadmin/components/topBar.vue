7eb005a030bb949211d703da08bebd62c6978e82
Caster418 <1051615128@qq.com>
2019-11-11 16:50:59 +0800

    多页面示例

---
+++ src/js/auditadmin/components/topBar.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="g-top">
+        <div class="pull-right">
+            <div class="t-msg">
+                <el-badge :value="12">
+                    <i class="el-icon-bell"></i>
+                </el-badge>
+            </div>
+            <el-dropdown>
+                <div class="t-usr">
+                    <img src=""/>欢迎
+                </div>
+                <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item disabled>
+                        <div class="m-drop-cell">
+                            <i class="el-icon-setting"></i> 设置
+                        </div>
+                    </el-dropdown-item>
+                    <el-dropdown-item divided>
+                        <div class="m-drop-cell" @click="handleExit">
+                            <i class="el-icon-delete"></i> 退出
+                        </div>
+                    </el-dropdown-item>
+                </el-dropdown-menu>
+            </el-dropdown>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'topBar',
+        methods: {
+            handleExit() {
+                window.location.href = './user';
+            }
+        }
+    }
+</script>
+
+<style lang="less">
+  .g-top {
+    width: 100%;
+    height: 60px;
+    color: rgba(0, 0, 0, .65);
+    box-shadow: 0 1px 4px rgba(0,21,41,.08);
+    padding: 0 25px;
+  .pull-right {
+    cursor: pointer;
+    float: right;
+    display: flex;
+    align-items: center;
+  .t-msg {
+    line-height: 24px;
+    font-size: 24px;
+    margin: 18px 30px;
+    display: inline-block;
+  }
+  .t-usr {
+    height: 24px;
+    line-height: 24px;
+    cursor: pointer;
+  img {
+    width: 24px;
+    vertical-align: middle;
+    margin-right: 8px;
+  }
+  }
+  }
+  }
+  .m-drop-cell {
+    font-size: 16px;
+    padding: 0 10px;
+  i {
+    margin-right: 10px;
+  }
+  }
+</style>
---
+++ src/js/auditadmin/home/home.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="box">
+        <div class="Absolute-Center" >
+            登录成功
+        </div>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        data() {
+            return {
+
+            }
+        },
+        methods: {
+        }
+    }
+</script>
+<style>
+  @import "../../../less/layout.css";
+        html,body{
+         margin: 0; padding: 0;
+        }
+        .box {
+            position: relative;
+            width: 600px;               /*宽*/
+            height: 400px;              /*高*/
+            margin: 0 auto;
+        }
+        .Absolute-Center {
+            font-size: 16px;
+            width: 160px; height: 80px;
+            position: relative;
+            top: 50%;
+            -moz-transform: translateY(-50%);  /*向上平移 自身的50%*/
+            -webkit-transform: translateY(-50%);
+            -ms-transform: translateY(-50%);
+            -o-transform: translateY(-50%);
+            transform: translateY(-50%);
+            margin: auto;
+        }
+
+</style>
