8988618a1c4625dcca8497edc591d5e0cc819be5
Caster418 <1051615128@qq.com>
2019-12-12 22:52:08 +0800

    审核负责人4

--- src/js/auditadmin/components/list/reimbursementRecord.vue
+++ src/js/auditadmin/components/list/reimbursementRecord.vue
@@ -263,7 +263,7 @@
         },
         methods: {
           timestampToTime(timestamp) {  
-            var now = new Date(timestamp * 1000);//时间戳为10位需*1000，时间戳为13位的话不需乘1000
+            var now = new Date(timestamp);//时间戳为10位需*1000，时间戳为13位的话不需乘1000
             var year=now.getFullYear();  //取得4位数的年份
 var month=now.getMonth()+1;  //取得日期中的月份，其中0表示1月，11表示12月
 var date=now.getDate();      //返回日期月份中的天数（1到31）
